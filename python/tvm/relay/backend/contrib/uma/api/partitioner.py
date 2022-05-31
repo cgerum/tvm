a609de4857f7ecc230a4f283b1a5ec9d37396bc3
Paul Palomero Bernardo <paulpb@outlook.com>
2022-05-31 20:30:30 +0200

    Initial UMA structure for version 1

---
+++ python/tvm/relay/backend/contrib/uma/api/partitioner.py
@@ -0,0 +1,86 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""Partitioner base class of the Universal Modular Accelerator Interface (UMA)"""
+
+from typing import Dict, List, Tuple, Optional
+
+import tvm
+from tvm import relay
+from tvm.relay.build_module import bind_params_by_name
+from tvm.relay.op.contrib.register import register_pattern_table
+from .utils import PassPhase
+
+
+class UMAPartitioner():
+    """Partitioner base class of the Universal Modular Accelerator Interface (UMA)."""
+
+    def __init__(self, target_name: str, merge_compiler_regions: bool = True) -> None:
+        self.target_name = target_name
+        self.merge_compiler_regions = merge_compiler_regions
+
+        self._relay_passes: List[Tuple[PassPhase, tvm.transform.Pass]] = []
+        self._patterns: List[Tuple[str, tvm.relay.dataflow_pattern.DFPattern]] = []
+
+    def _pattern_table(self):
+        return [(self.target_name + "." + pattern[0], pattern[1]) for pattern in self._patterns]
+
+    def register(self) -> None:
+        """Register all relevant relay-to-relay functions."""
+        register_pattern_table(self.target_name, self._pattern_table)
+
+    def partition(
+        self, mod: tvm.IRModule, params: Optional[Dict[str, tvm.runtime.NDArray]] = None
+    ) -> tvm.IRModule:
+        """Partition the relay graph in by the NPU supported and unsupported parts.
+
+        Parameters
+        ----------
+        mod : tvm.IRModule
+            The relay module to be partitioned.
+
+        Returns
+        -------
+        out : tvm.IRModule
+            The partitioned relay module.
+
+        """
+        if params:
+            mod["main"] = bind_params_by_name(mod["main"], params)
+
+        mod = relay.transform.InferType()(mod)
+        mod = tvm.transform.Sequential(
+            [p[1] for p in self._relay_passes if p[0] == PassPhase.PRE_PARTITIONING]
+        )(mod)
+        mod = relay.transform.MergeComposite(self._pattern_table())(mod)
+        mod = relay.transform.AnnotateTarget(self.target_name)(mod)
+        if self.merge_compiler_regions:
+            mod = relay.transform.MergeCompilerRegions()(mod)
+        mod = relay.transform.InferType()(mod)
+        mod = relay.transform.PartitionGraph()(mod)
+        mod = relay.transform.InferType()(mod)
+        mod = tvm.transform.Sequential(
+            [p[1] for p in self._relay_passes if p[0] == PassPhase.POST_PARTITIONING_0]
+        )(mod)
+        mod = relay.transform.InferType()(mod)
+        # Defunctionalize the partitioned functions to allow lowering
+        for gv, func in mod.functions.items():
+            mod.update_func(gv, relay.transform.Defunctionalization(func, mod))
+        mod = tvm.transform.Sequential(
+            [p[1] for p in self._relay_passes if p[0] == PassPhase.POST_PARTITIONING_1]
+        )(mod)
+
+        return mod
---
+++ src/relay/backend/contrib/uma/targets.cc
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*!
+ * \file relay/backend/contrib/uma/targets.cc
+ *
+ * \brief this file contains the targets for the Universal Modular Accelerator Interface (UMA).
+ */
+
+#include <tvm/relay/transform.h>
+#include <tvm/target/target.h>
+
+namespace tvm {
+
+namespace relay {
+namespace contrib {
+namespace uma {
+    tvm::transform::Pass RelayToTIR(String target_name);
+    runtime::Module TIRToRuntime(IRModule mod, Target target);
+}  // namespace uma
+}  // namespace contrib
+}  // namespace relay
+
+TVM_REGISTER_GLOBAL("relay.backend.contrib.uma.RegisterTarget")
+    .set_body_typed([](String target_name, Map<String, ObjectRef> attr_options){
+        auto target_kind = ::tvm::TargetKindRegEntry::RegisterOrGet(target_name)
+        .set_name()
+        .set_device_type(kDLCPU)
+        .add_attr_option<Array<String>>("keys")
+        .add_attr_option<String>("tag")
+        .add_attr_option<String>("device")
+        .add_attr_option<String>("model")
+        .add_attr_option<Array<String>>("libs")
+        .add_attr_option<Target>("host")
+        .add_attr_option<Integer>("from_device")
+        .set_attr<FTVMRelayToTIR>("RelayToTIR", relay::contrib::uma::RelayToTIR(target_name))
+        .set_attr<FTVMTIRToRuntime>("TIRToRuntime", relay::contrib::uma::TIRToRuntime);
+
+        for (auto &attr_option : attr_options) {
+          try {
+            target_kind.add_attr_option<String>(attr_option.first, Downcast<String>(attr_option.second));
+            continue;
+          } catch (...) {}
+          try {
+            target_kind.add_attr_option<Bool>(attr_option.first, Downcast<Bool>(attr_option.second));
+            continue;
+          } catch (...) {}
+          try {
+            target_kind.add_attr_option<Integer>(attr_option.first, Downcast<Integer>(attr_option.second));
+            continue;
+          } catch (...) {
+            LOG(FATAL) << "Attribute option of type " << attr_option.second->GetTypeKey() 
+                       << " can not be added. Only String, Integer, or Bool are supported.";
+          }
+        }
+    });
+
+}  // namespace tvm
